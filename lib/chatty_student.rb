ed40951525c2289ed9f0d3c6ace5ecdcbb5894db
Evan Burton <evan@heyevan.com>
2020-05-27 04:58:04 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,7 +7,16 @@ class ChattyStudent < Student
 
   def raise_hand
     super
-    10.times {puts "Pick me!"}
+    super
+    super
+    super
+    super 
+    super 
+    super 
+    super 
+    super 
+    super 
+    puts "Pick me!" "
   end
 
 end
